8600154da9263a1e775fb778a5fa4dbbce3e0391
Cong Le <congldqn888@gmail.com>
2024-02-06 10:32:42 +0700

    Test App render and React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { useContext, useEffect } from 'react'
+import { HelmetProvider } from 'react-helmet-async'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import { AppContext } from './contexts/app.context'
+import 'src/i18n/i18n'
+import ErrorBoundary from './components/ErrorBoundary'
+import { AppContext, AppProvider } from './contexts/app.context'
 import useRouteElements from './useRouteElements'
 import { LocalStorageEventTarget } from './utils/auth'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 0
+    }
+  }
+})
 
 function App() {
   const routeElements = useRouteElements()
@@ -19,8 +33,17 @@ function App() {
 
   return (
     <div className='font-Inter'>
-      {routeElements}
-      <ToastContainer />
+      <HelmetProvider>
+        <QueryClientProvider client={queryClient}>
+          <AppProvider>
+            <ErrorBoundary>
+              {routeElements}
+              <ToastContainer />
+            </ErrorBoundary>
+          </AppProvider>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </HelmetProvider>
     </div>
   )
 }
